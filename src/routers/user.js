3e61f52a19f87b6116c32e38daf40d2296ffca65
Ndemco <50844654+Ndemco@users.noreply.github.com>
2020-10-23 12:50:49 -0400

    generate auth token for log in and sign up routes

--- src/routers/user.js
+++ src/routers/user.js
@@ -3,8 +3,7 @@ const User = require('../models/users')
 const router = new express.Router()
 
 /*
-post() method
-request from front-end to create data on back-end
+this function is for signing up users
 @param req: the data to be posted
 @param res: the response, either the successfully posted data, or an error
 */
@@ -13,16 +12,19 @@ router.post('/users', async(req, res) => {
 
     try {
         await user.save()
-        res.status(201).send(user)
+        const token = await user.generateAuthToken()
+        res.send({ user, token })
     } catch (e) {
         res.status(400).send(e)
     }
 })
 
+// logging in users
 router.post('/users/login', async(req, res) => {
     try {
         const user = await User.findByCredentials(req.body.email, req.body.password)
-        res.send(user)
+        const token = await user.generateAuthToken()
+        res.send({ user, token })
     } catch (e) {
         res.status(400).send()
     }
