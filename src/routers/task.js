53e3df1a4b77af5b75384b1b8f250f9220be8555
Nicholas DeMarco <50844654+Ndemco@users.noreply.github.com>
2020-10-24 00:35:15 -0400

    implement support for pagination

--- src/routers/task.js
+++ src/routers/task.js
@@ -21,19 +21,23 @@ router.post('/tasks', auth, async(req, res) => {
 })
 
 //get all tasks
+//GET /tasks?completed=true
+//GET /tasks?limit=10&skip=0
 router.get('/tasks', auth, async(req, res) => {
     const match = {}
 
     if (req.query.completed) {
         match.completed = req.query.completed === 'true'
-    } else {
-        match.completed = false
     }
 
     try {
         await req.user.populate({
             path: 'tasks',
-            match
+            match,
+            options: {
+                limit: parseInt(req.query.limit),
+                skip: parseInt(req.query.skip)
+            }
         }).execPopulate()
         res.send(req.user.tasks)
     } catch (e) {
