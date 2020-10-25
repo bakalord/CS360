d63f81fae39aa81768130f311545dbf0a44d6c58
HudsonFleur <58185069+HudsonFleur@users.noreply.github.com>
2020-10-24 20:11:45 -0400

    Deleted Nick's backend stuff

--- src/db/mongoose.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-// Connects to the mongdb database
-mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false
-})
