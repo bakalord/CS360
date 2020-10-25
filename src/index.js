2292be9afa111b9344006009bf0e2b082101b404
Nicholas DeMarco <50844654+Ndemco@users.noreply.github.com>
2020-10-25 00:36:03 -0400

    comment

--- src/index.js
+++ src/index.js
@@ -4,10 +4,13 @@ const userRouter = require('./routers/user')
 const taskRouter = require('./routers/task')
 
 const app = express()
+
+//whatever is in the environment variable "PORT", or localhost if nothing is there
 const port = process.env.PORT || 3000
 
 //this tells the app to automatically parse incoming JSON to an object
 app.use(express.json())
+
 
 app.use(userRouter)
 app.use(taskRouter)
